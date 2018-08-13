cdcc21016cbd68de440dde2d61f1cc1a815ba060
zafar-hussain <zafar_hussain2000@hotmail.com>
2018-08-13 12:26:21 +0500

    gps init

---
+++ gps.java
@@ -0,0 +1,15 @@
+ #lang profj/beginner
+ 
+// first commit
+
+// GPS locations
+// GPSlocation is a struct : int latitude int longitude
+class GPSlocation {
+    double latitude;            // degrees
+    double longitude;           //degrees
+
+    GPSlocation (double latitude, double longitude){
+        this.latitude  = latitude;
+        this.longitude = longitude;
+    }
+}
