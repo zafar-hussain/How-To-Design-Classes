27b282f762182674e976268065639b22c62ecc90
zafar-hussain <zafar_hussain2000@hotmail.com>
2018-08-13 12:20:02 +0500

    gps class

--- first-commit.java
+++ first-commit.java
@@ -1,1 +1,14 @@
 // first commit
+
+// GPS locations
+// GPSlocation is a struct : int latitude int longitude
+class GPSlocation {
+    double latitude;            // degrees
+    double longitude;           //degrees
+
+    GPSlocation (double latitude, double longitude){
+        this.latitude  = latitude;
+        this.longitude = longitude;
+    }
+}
+
