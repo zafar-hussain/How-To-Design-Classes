66fcb23346bf168a1edf30abffbb85621ded494d
zafar-hussain <zafar_hussain2000@hotmail.com>
2018-08-14 14:11:57 +0500

    gitignore modified

--- library.java
+++ library.java
@@ -112,3 +112,9 @@ class Automobile
   }
  }
 
+ public class HelloWorld {
+     public static void main(String[] args) {
+         // Prints "Hello, World" in the terminal window.
+         System.out.println("Hello, World");
+     }
+ }
---
+++ trains.java
@@ -0,0 +1,16 @@
+// // . . . Develop a program that can assist railway travelers with
+// the arrangement of train trips. The available information about
+// a specific train includes its schedule, its route, and whether it is
+// local. (The opposite is an express train.) The route information
+// consists of the origin and the destination station. A schedule
+// // specifies the departure and the arrival times. . . .
+
+
+
+
+public class HelloWorld {
+     public static void main(String[] args) {
+         // Prints "Hello, World" in the terminal window. 
+         System.out.println("Hello, World");
+     }
+ }
