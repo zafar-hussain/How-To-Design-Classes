4f7997a272fb51a382c9b416ed92b1f1f6bde0ba
zafar-hussain <zafar_hussain2000@hotmail.com>
2018-08-22 15:45:00 +0500

    coffee

---
+++ Coffee.java
@@ -0,0 +1,26 @@
+/*Coffee is a structure - kind, price, weight
+-- (make-Coffee (string, int, int)
+ */
+
+
+public class Coffee
+{
+    String kind;
+    int price;
+    int weight;
+
+
+    Coffee (String kind, int price, int weight)
+    {
+        this.kind   = kind;
+        this.price  = price;
+        this.weight = weight;
+    }
+
+
+public static void main (String[] args)
+    {
+        System.out.println("hi there");
+    }
+
+}
