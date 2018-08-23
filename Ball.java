ca2634599d30c29fd92389911ec3c7fc651f712e
zafar-hussain <zafar_hussain2000@hotmail.com>
2018-08-23 11:58:24 +0500

    coffee examples

--- Ball.java
+++ Ball.java
@@ -16,15 +16,15 @@ public class Ball
 {
     int x = 0;      // x coordinate of ball
     int y = 0;      // y coordinate of ball
-    int radius = 0; // radius of the ball
+    int radius = 5; //constant radius of the ball
 
     //constructor
     Ball (int x, int y)
     {
         this.x = x;
         this.y = y;
-        radius = 5;   //constant
     }
+
 public static void main (String[] args)
     {
         System.out.println("hi there");
---
+++ CoffeeExamples.java
@@ -0,0 +1,24 @@
+/*
+class to hold coffee examples
+ */
+
+public class CoffeeExamples
+{
+    Coffee kona = new Coffee(
+            "kona",
+            2095,
+            100);
+    Coffee ethi = new Coffee(
+            "Ethiopian",
+            800,
+            1000);
+    Coffee colo = new Coffee(
+            "colombian",
+            950,
+            20);
+
+    public static void main (String[] args)
+    {
+        System.out.println("Coffee Examples");
+    }
+}
