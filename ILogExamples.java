e338bf0d5389d57e93bc7067f5a21798a4dd3e94
zafar-hussain <zafar_hussain2000@hotmail.com>
2018-08-28 00:47:47 +0500

    Ilog implementation

--- ILogExamples.java
+++ ILogExamples.java
@@ -13,4 +13,23 @@ public class ILogExamples
     ILog l3 = new ConsLog(e2, l2);
     ILog l4 = new ConsLog(e3, l3);
 
+    /*
+        Exercise 5.1 Translate these two objects of type ILog
+        ILog l5 = new ConsLog(e3,l1);
+        ILog l6 = new ConsLog(e3,l2);
+        into the runner’s world of logs. Assume these examples were constructed
+        in the context of the four examples above
+     */
+    ILog l5 = new ConsLog(e3, l1);
+    ILog l6 = new ConsLog(e3, l2);
+
+    /*
+        Exercise 5.2 Represent the following runner’s log as objects:
+        1. on June 15, 2004: 15.3 miles in 87 minutes, feeling great;
+        2. on June 16, 2004: 12.8 miles in 84 minutes, feeling good;
+        3. on June 23, 2004: 26.2 miles in 250 minutes, feeling dead;
+        4. on June 28, 2004: 26.2 miles in 150 minutes, good recovery
+    */
+
+
 }
