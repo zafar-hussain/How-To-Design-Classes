20510c29d1b576df4d3fad479c5d1b4410570a52
zafar-hussain <zafar_hussain2000@hotmail.com>
2018-08-29 21:42:40 +0500

    POSN STRUCT

---
+++ Posn.java
@@ -0,0 +1,52 @@
+/*
+Class Posn is a compound structure
+- Posn -> make-struct x y
+- posn -> int int
+
+(fn-for-Posn p
+(
+    cond
+        [(empty? p) (...)]
+        [(Posn? p)
+            ... (Posn-x p)
+            ... (Posn-y p)
+        ]
+)
+*/
+public class Posn
+{
+    int x;
+    int y;
+
+    public Posn(int x, int y)
+    {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX()
+    {
+        return x;
+    }
+
+    public int getY()
+    {
+        return y;
+    }
+
+    public double distanceToZero()
+    {
+        return Math.sqrt((getX() * getX()) + (getY() * getY()));
+    }
+
+
+    public static void main()
+    {
+        Posn a = new Posn(1, 2);
+        System.out.println(a.distanceToZero());
+
+    }
+
+}
+
+
