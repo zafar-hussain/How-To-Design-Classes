2c124ef2cec705178ef93414e004c2018defc920
zafar-hussain <zafar_hussain2000@hotmail.com>
2018-08-30 18:39:30 +0500

    Empty frame

--- DrawFrame.java
+++ DrawFrame.java
@@ -1,15 +1,14 @@
-import javax.swing.*;
 
-// draws a frame to depict graphics
+
+
 public class DrawFrame
 {
     public static void main(String[] args)
     {
-        JFrame frame = new JFrame();
-        frame.setSize(300, 300);
-        frame.setTitle("An Empty frame");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setVisible(true);
+        double t = Math.sqrt(3.0) / 2.0;
+        StdDraw.line(0.0, 0.0, 1.0, 0.0);
+        StdDraw.line(1.0, 0.0, 0.5, t);
+        StdDraw.line(0.5, t, 0.0, 0.0);
+        StdDraw.point(0.5, t / 3.0);
     }
-
 }
