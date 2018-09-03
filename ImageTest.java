93871f1047f7423bfe822f765a043e64284b2a81
zafar-hussain <zafar_hussain2000@hotmail.com>
2018-09-03 16:34:02 +0500

    Image methods

--- ImageTest.java
+++ ImageTest.java
@@ -4,9 +4,9 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class ImageTest
 {
-    Image baseCase = new Image(0, 0, "", "");
-    Image aPortrait = new Image(100, 50, "test", "my house");
-    Image aLandScape = new Image(50, 100, "hi", "low");
+    private Image baseCase = new Image(0, 0, "", "");
+    private Image aPortrait = new Image(100, 50, "test", "my house");
+    private Image aLandScape = new Image(50, 100, "hi", "low");
 
     @Test
     void isPortrait()
